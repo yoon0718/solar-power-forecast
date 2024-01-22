022f60b71e9a3b20abaf140e988efaaf5cfb4f46
ny_980321 <ny_980321@naver.com>
2024-01-22 11:11:36 +0900

    푸터 추가

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
     return (
     <div>
+        <footer>
+            <h5 id="path">오시는 길 : 광주광역시 광산구 소촌로 152번길 37</h5><br/>
+            <h5>깃허브 주소 : <a href='https://github.com/yoon0718/solar-power-forecast'>https://github.com/yoon0718/solar-power-forecast</a></h5>
+            <h5>깃허브 아이디 : yoon0718(PL), Jssong-ho, raincross7, portk, azure0321</h5>
+        </footer>
     </div>
     )
 }
